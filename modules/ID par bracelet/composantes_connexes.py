a156eb4dd92190de5415e225e14c7b36cd66d92c
Adrien Maes <adrien.maes@telecom-paris.fr>
2019-12-11 18:11:04 +0100

    table de hashage

--- modules/ID par bracelet/composantes_connexes.py
+++ modules/ID par bracelet/composantes_connexes.py
@@ -19,3 +19,29 @@ def verif_image(ima):
             if not (val == 1 or val == 0):
                 return False
     return True
+
+def HashCreation():
+    return []
+
+def HashAjout(table, cle, elem):
+    table.append([cle, elem])
+
+def HashRecup(table, cle):
+    for elem in table:
+        if elem[0] == cle:
+            return elem[1]
+
+def HashExiste(table, cle):
+    for elem in table:
+        if elem[0] == cle:
+            return True
+
+def HashModif(table, cle, newVal):
+    for elem in table:
+        if elem[0] == cle:
+            elem[1] = newVal
+
+def HashSuppr(table, cle):
+    for i in range(len(table)):
+        if table[i][0] == cle:
+            table.pop(i)
