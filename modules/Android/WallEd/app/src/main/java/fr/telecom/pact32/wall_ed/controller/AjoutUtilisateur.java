791cf3834f1c66c7988d4e87affeef2820be7af5
Lola Vitrac <lola.vitrac@telecom-paris.fr>
2019-12-16 15:56:53 +0100

    déclaration variables ajout utilisateurs

--- modules/Android/WallEd/app/src/main/java/fr/telecom/pact32/wall_ed/controller/AjoutUtilisateur.java
+++ modules/Android/WallEd/app/src/main/java/fr/telecom/pact32/wall_ed/controller/AjoutUtilisateur.java
@@ -1,14 +1,35 @@
 package fr.telecom.pact32.wall_ed.controller;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import fr.telecom.pact32.wall_ed.R;
 
 public class AjoutUtilisateur extends AppCompatActivity {
+
+    private TextView enteteText ;
+    private EditText prenomInput ;
+    private EditText nomInput ;
+    private EditText classeInput ;
+    private Button okEnregistrer ;
+    private ImageButton prendrePhoto ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_utilisateur);
+
+        enteteText = (TextView) findViewById(R.id.textView2);
+        prenomInput = (EditText) findViewById(R.id.prenom_txt);
+        nomInput = (EditText) findViewById(R.id.Nom_txt);
+        classeInput = (EditText) findViewById(R.id.classe_txt);
+        okEnregistrer = (Button) findViewById(R.id.enregistrement_button);
+        prendrePhoto = (ImageButton) findViewById(R.id.imageButton3);
+
     }
 }
