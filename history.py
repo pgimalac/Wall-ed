3da3b25b88b27db24fb7a863c62e6101ff257090
Nico <nicojow@hotmail.fr>
2020-01-06 16:25:03 +0100

    added algorithm Filtre sur image

--- history.py
+++ history.py
@@ -8,3 +8,7 @@ pip install tensorflow
 
 ## ---(Mon Dec  2 15:28:47 2019)---
 runfile('C:/Users/Nico/.spyder/temp.py', wdir='C:/Users/Nico/.spyder')
+
+## ---(Mon Dec  9 16:11:34 2019)---
+runfile('C:/Users/Nico/Documents/Python Scripts/Detection de carré colorés.py', wdir='C:/Users/Nico/Documents/Python Scripts')
+import cv2
