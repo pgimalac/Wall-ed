1f168ebdde89eb32ecaf17e0a6a65ab5658a903d
Lola Vitrac <lola.vitrac@telecom-paris.fr>
2020-01-14 16:14:14 +0100

    ajoût de nouveaux fragments (statistiques globales et individuelles)

---
+++ modules/Android/WallEd/app/src/main/java/fr/telecom/wall_ed/Statistiques_individuelles.java
@@ -0,0 +1,31 @@
+package fr.telecom.wall_ed;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Statistiques_individuelles extends Fragment {
+
+
+    public Statistiques_individuelles() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistiques_individuelles2, container, false);
+    }
+
+}
