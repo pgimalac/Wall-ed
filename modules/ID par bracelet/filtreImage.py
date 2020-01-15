cdcd8a633ea8eaca87a9bf7cac73025ccee61d9e
Adrien MAES <adrien.maes@telecom-paris.fr>
2020-01-15 15:44:00 +0100

    code du main, ready for testing

--- modules/ID par bracelet/filtreImage.py
+++ modules/ID par bracelet/filtreImage.py
@@ -5,13 +5,13 @@
 Ceci est un script temporaire.
 """
 
-import cv2 
+import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
 
-    
+
 def seuillageGris(img, seuil):
     image = Image.open(img)
     ImageTab = np.array(image)
@@ -24,16 +24,9 @@ def seuillageGris(img, seuil):
             else :
                 imgHSV[i][j][2] = 255
                 imgHSV[i][j][1] = 0
-    img2 = cv2.cvtColor(imgHSV, cv2.COLOR_HSV2RGB)     
+    img2 = cv2.cvtColor(imgHSV, cv2.COLOR_HSV2RGB)
     return img2
 
-
-
-
-resultat = seuillageGris("dechet-menager.jpg", 220)
-plt.imshow(resultat)
-plt.show()
-
-
-
-
+#resultat = seuillageGris("dechet-menager.jpg", 220)
+#plt.imshow(resultat)
+#plt.show()
