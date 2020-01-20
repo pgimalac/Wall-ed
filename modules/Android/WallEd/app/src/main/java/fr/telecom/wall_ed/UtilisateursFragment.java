829210df9ef4e96751902f97c9f75adbc64137f7
Lola Vitrac <lola.vitrac@telecom-paris.fr>
2020-01-20 15:32:53 +0100

    interface pour callBack gestion utilisateur

--- modules/Android/WallEd/app/src/main/java/fr/telecom/wall_ed/UtilisateursFragment.java
+++ modules/Android/WallEd/app/src/main/java/fr/telecom/wall_ed/UtilisateursFragment.java
@@ -45,6 +45,8 @@ public class UtilisateursFragment extends Fragment  {
     }
 
 
+
+
 }
 
 
---
+++ modules/Android/WallEd/app/src/main/java/fr/telecom/wall_ed/model/InterfaceGestionUtilisateurs.java
@@ -0,0 +1,12 @@
+package fr.telecom.wall_ed.model;
+import java.util.ArrayList;
+
+import fr.telecom.pact32.wall_ed.model.Utilisateur;
+
+public interface InterfaceGestionUtilisateurs {
+
+    public ArrayList<Utilisateur> GetUser ();
+    public void SetUser(); //modifier les paramètres d'un
+
+
+}
