7a8716d3e5826994fe545b83a378461c476535ed
Adrien MAES <adrien.maes@telecom-paris.fr>
2020-03-04 21:25:25 +0100

    client python

---
+++ modules/Communication client-serveur/clientRobot.py
@@ -0,0 +1,18 @@
+import socket
+import pickle
+
+hote = "adresseIP"
+port = 8000
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def initConnexion():
+	socket.connect((hote,port))
+	liste_eleves = socket.recv(255)
+	return json.loads(liste_eleves)
+
+def sendSomething(something):
+	socket.send(pickle.dumps(something))
+
+def stopConnexion():
+	socket.close()
