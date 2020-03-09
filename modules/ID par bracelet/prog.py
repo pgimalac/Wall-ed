b0e1bdb1f0c2e62c8f7f8bb02edf75d77cf5b750
Nico <nicojow@hotmail.fr>
2020-03-09 15:26:07 +0100

    deleting quicksort

--- modules/ID par bracelet/prog.py
+++ modules/ID par bracelet/prog.py
@@ -72,31 +72,7 @@ def trouverCouleurImg(img,Color):
     elif main(img)[1] == "ROUGE":
         Color[0][1] = Color[5][1] + 1
         
-def trirapide(L):
-    
-    def trirap(L, g, d):
-        pivot = L[(g+d)//2]
-        i = g
-        j = d
-        while True:
-            while L[i]<pivot:
-                i+=1
-            while L[j]>pivot:
-                j-=1
-            if i>j:
-                break
-            if i<j:
-                L[i], L[j] = L[j], L[i]
-            i+=1
-            j-=1
-        if g<j:
-            trirap(L,g,j)
-        if i<d:
-            trirap(L,i,d)
- 
-    g=0
-    d=len(L)-1
-    trirap(L,g,d)
+        
     
     
 def main(img):
