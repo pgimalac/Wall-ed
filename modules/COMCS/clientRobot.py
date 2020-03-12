7e1bf60d90df1dd52b904f9733cbcfdacf620f98
Florian <florian.lemouel@telecom-paris.fr>
2020-03-12 15:06:10 +0100

    minor test changes

--- modules/COMCS/clientRobot.py
+++ modules/COMCS/clientRobot.py
@@ -3,9 +3,10 @@ import pickle
 import json
 import imageio
 import numpy
+import time
 
-hote = "192.168.2.6"
-port = 2346
+hote = "192.168.2.4"
+port = 22346
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -15,9 +16,16 @@ def initConnexion():
 	liste_eleves = socket.recv(255)
 	return json.loads(liste_eleves)
 
-def sendFile(filepath):
+def sendImage(filepath):
+	socket.send(bytes("newImage",'utf-8'))
+	print("sent command")
+	time.sleep(5)
+	print(socket.recv(255))
+	time.sleep(5)
 	file = open(filepath,'rb')
-	socket.sendfile(file)
+	socket.send(pickle.dumps(file))
+	print("sent image")
+	time.sleep(5)
 	return json.loads(socket.recv(255))
 
 def stopConnexion():
