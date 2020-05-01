14fa76caa0a16df31d5afc24d8d124aed0d3c447
Romain <romain.louvet@telecom-paris.fr>
2020-05-01 15:33:33 +0200

    Gestion du regard

---
+++ modules/Conception visuelle & audio/lectreg3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 10:52:28 2020
+
+@author: romain
+"""
+
+
+import numpy as np
+import cv2
+
+def lire(chn=0):
+
+    cap = cv2.VideoCapture('regard3.mkv')
+    cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("frame",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    
+    if (cap.isOpened() == False):
+        print("Error opening video file")
+        
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+    
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    lire()
