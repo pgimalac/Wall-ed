1ef6a11dde4a02d43c2fe1e02e6ad518b854664d
Adrien MAES <adrien.maes@telecom-paris.fr>
2020-05-30 12:30:28 +0200

    modifs results

--- modules/COMCS/clientRobot.py
+++ modules/COMCS/clientRobot.py
@@ -20,18 +20,18 @@ def sendImage(filepath):
     data = file.read()
     size = len(data)
     size.to_bytes(8, byteorder = 'big')
-    print(size)
     socket2.sendall(str(size).encode())
     socket2.sendall(data)
     socket2.send(bytes("False", 'utf-8'))
     print("image sent")
-    print(socket2.recv(255))
+    res = socket2.recv(255)
     socket2.close()
+    return res
     #attention le serveur attend une réponse, si un déchet à été instancié
     # ---> envoyer un JSON sous la forme : {"trashFound" : boolean, "braceletID" : ID, "type" : type_dechet, "typePropose" : type_propose_par_robot(coque), "reponseEleve" : boolean}
 
 def interactionAnswer(trashFound, braceletID, type, typePropose, reponseEleve):
-    socket.send(bytes("newImage", 'utf-8')))
+    socket.send(bytes("newImage", 'utf-8'))
     socket.send(bytes(json.dumps({'trashFound' : trashFound, 'braceletID' : braceletID, 'type' : type, 'typePropose' : typePropose, 'reponseEleve' : reponseEleve}), 'utf-8'))
 
 def stopConnexion():
